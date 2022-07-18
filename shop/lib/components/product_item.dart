0aabf192eef2f07b2ec9e41d6c0721239b83ea46
CaioNoboa <caiosalvador@hotmail.com>
2022-07-18 11:57:19 -0300

    showFavoriteOnly-product_list

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -6,7 +6,11 @@ import 'package:shop/utils/app_routes.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
+    // o padrão é listen: true, para que este componente esteja 'escutando' as mudanças de estado. Ao colocarmos false, podemos usar o Consumer apenas na parte do código que a interface será alterada pela mudança de estado. Assim, otimizamos a aplicação, pois os componentes que não sofrerão alteração na interface não serão renderizados novamente.
 
     return ClipRRect(
       // corta de forma arredondada determinado elemento
@@ -26,12 +30,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black54,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (context1, product, _) => IconButton(
+              // coloquei context1 para usar o contexto lá do main. Se colocar apenas context, a cor do ícone será alterada, pois utilizará o BuildContext desse arquivo
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
           title: Text(
             product.title,
