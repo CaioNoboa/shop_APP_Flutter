bedd9b019565992529fd71a0c74bc6b6144f8920
Caio Salvador Noboa <caiosalvador@hotmail.com>
2022-07-28 11:25:44 -0300

    utilizando-future-builder

--- shop/lib/pages/orders_page.dart
+++ shop/lib/pages/orders_page.dart
@@ -4,23 +4,17 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
+  // bool _isLoading = true;
 
-  @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(context, listen: false).loadOrders().then((value) {
-      setState(() => _isLoading = false);
-    });
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Provider.of<OrderList>(context, listen: false).loadOrders().then((value) {
+  //     setState(() => _isLoading = false);
+  //   });
+  // }
 
   Future<void> _refreshOrders(BuildContext context) {
     return Provider.of<OrderList>(context, listen: false).loadOrders();
@@ -28,23 +22,47 @@ class _OrdersPageState extends State<OrdersPage> {
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
+    // final OrderList orders = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus pedidos'),
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: () => _refreshOrders(context),
-              child: ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (context, index) {
-                  return OrderWidget(order: orders.items[index]);
-                },
-              ),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orders, child) {
+                return RefreshIndicator(
+                  onRefresh: () => _refreshOrders(context),
+                  child: ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (context, index) {
+                      return OrderWidget(order: orders.items[index]);
+                    },
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+
+      // body: _isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : RefreshIndicator(
+      //         onRefresh: () => _refreshOrders(context),
+      //         child: ListView.builder(
+      //           itemCount: orders.itemsCount,
+      //           itemBuilder: (context, index) {
+      //             return OrderWidget(order: orders.items[index]);
+      //           },
+      //         ),
+      //       ),
+
       drawer: AppDrawer(),
     );
   }
