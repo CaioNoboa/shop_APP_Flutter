0f6b4eea2d079ce6ed90e47a3a2235d0c0faeecb
Caio Salvador Noboa <caiosalvador@hotmail.com>
2022-08-02 16:36:20 -0300

    main

--- shop/lib/models/auth.dart
+++ shop/lib/models/auth.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/data/store.dart';
 import 'package:shop/exceptions/auth_exception.dart';
+import 'package:shop/utils/key.dart';
 
 class Auth with ChangeNotifier {
   String? _token;
@@ -33,7 +34,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyD-KR2FOjieq_1yTWL9xYR6BgQvlFur6N4';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?${UrlKey.keyBaseUrl}';
     final response = await http.post(
       Uri.parse(url),
       body: jsonEncode({
